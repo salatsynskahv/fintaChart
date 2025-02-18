e1767f6c64d3ae04a200d7ae19c7cf12756d3105
salatsynskahv <salatsynskahv@gmail.com>
2025-02-18 10:07:44 +0200

    fix cors for github pages

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -26,7 +26,11 @@ export class AuthService {
     body.set('password', environment.PASSWORD);
 
     const headers = new HttpHeaders({
-      'Content-Type': 'application/x-www-form-urlencoded',
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Methods': 'GET, POST, OPTIONS, PUT, PATCH, DELETE',
+      'Access-Control-Allow-Credentials': 'true',
+      'Access-Control-Allow-Headers': 'X-Requested-With,content-type',
     });
 
     this.http.post(url, body.toString(), {headers, withCredentials: true}).subscribe((result) => {
